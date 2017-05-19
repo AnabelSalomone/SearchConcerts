68dd2c2caa7044c90a75e0d87d1990874c5053ce
AnabelSalomone <anabel.salomone@gmail.com>
2017-05-19 10:29:35 +0200

    first commit. Navbar without functionalities

---
+++ src/Store.js
@@ -0,0 +1,81 @@
+export const Store = {
+	datas: {
+		shows: [
+			{
+				id: 1,
+				titre: "Concert Benjamin Biolay",
+				lieu: {
+					nom: "Halle Tony Garnier",
+					addresse: "20 place Docteurs Mérieux",
+					arrondissement: "69007"
+				},
+				type: "concert",
+				date: "23 Mai 2017",
+				prix: 20
+			},
+			{
+				id: 2,
+				titre: "Concert Air",
+				lieu: {
+					nom: "Marché Gare",
+					addresse: "34 Rue Casimir Périer",
+					arrondissement: "69002"
+				},
+				type: "concert",
+				date: "04 Juin 2017",
+				prix: 35
+			},
+			{
+				id: 3,
+				titre: "Opera Nabucco",
+				lieu: {
+					nom: "Opera de Lyon",
+					addresse: "1 Place de la Comédie",
+					arrondissement: "69001"
+				},
+				type: "Concert",
+				date: "18 Juin 2017",
+				prix: 50
+			},
+			{
+				id: 4,
+				titre: "Le malade imaginaire",
+				lieu: {
+					nom: "Le Rideau Rouge",
+					addresse: "1 place Marcel Bertone",
+					arrondissement: "69004"
+				},
+				type: "theatre",
+				date: "18 Juin 2017",
+				prix: 50
+			},
+			{
+				id: 5,
+				titre: "Le roi a la triste figure",
+				lieu: {
+					nom: "Les Celestins",
+					addresse: "4 Rue Charles Dullin",
+					arrondissement: "69002"
+				},
+				type: "theatre",
+				date: "18 Juin 2017",
+				prix: 50
+			},
+			{
+				id: 6,
+				titre: "Al Andalus Flamenco Nuevo",
+				lieu: {
+					nom: "Maison de la Danse",
+					addresse: "8 Avenue Jean Mermoz, 69008 Lyon",
+					arrondissement: "69008"
+				},
+				type: "danse",
+				date: "25 Juin 2017",
+				prix: 40
+			},
+		],
+
+
+		search: ''
+	}
+};
---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+<nav>
+    <div class="nav-wrapper red">
+      <a href="#" class="brand-logo"><img src="http://greetings2015.onlylyon.com/static/img/big_logo_only1.png" style="height: 50px;;"></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down black-text">
+			<li><form class="col s4">
+        <div class="input-field col s12">
+          <i class="material-icons prefix">account_circle</i>
+          <input id="icon_prefix" type="text" class="validate" v-model="datas.search">
+          <label for="icon_prefix" v-if="search.length === 0"></label>
+        </div>
+				</form></li>
+        <li class ="black-text"><a href="sass.html">Musique</a></li>
+        <li class ="black-text"><a href="badges.html">Thatre</a></li>
+        <li class ="black-text"><a href="collapsible.html">Danse</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+import {Store} from '@/Store.js'
+
+
+export default {
+	name: 'myForm',
+	data(){
+    return {
+			datas: Store.datas,
+			search: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul li a{
+  color: black;
+ }
+</style>
